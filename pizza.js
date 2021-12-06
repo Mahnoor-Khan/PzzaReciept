13e08cc6ccc950e9ced2da4b65302008309a31ee
Mahnoor Khan <mahnoorza001@gmail.com>
2021-12-06 18:41:43 +0800

    ChangeReceipt

--- pizza.js
+++ pizza.js
@@ -12,7 +12,6 @@ const modal=document.getElementById('myModal');
 const orderModal=document.getElementById('OrderModal');
 const btn =document.getElementsByClassName('OrderBtn');
 const closeBtn=document.getElementsByClassName('close')[0];
-const closeBtn2=document.getElementsByClassName('close2')[0];
 const modalHeading=document.getElementsByClassName('modal_heading')[0];
 const Total=document.getElementsByClassName('total')[0];
 const desHeading=document.getElementsByClassName('Heading')[0];
@@ -21,33 +20,77 @@ const selectionData=document.getElementsByClassName('selection-data')[0];
 const replaceData=document.getElementsByClassName('ReplaceData')[0];
 const disCodeInput=document.getElementById('codeInput');
 const disCodeSection=document.getElementsByClassName('discountCodeSection')[0];
+const tenPercntDiscount=document.getElementById('tenPerDisBtn');
+const twntyPercntDiscount=document.getElementById('twntyPerDisBtn');
+const code=document.getElementsByClassName('code')[0];
+const ExtraCheese=document.querySelector('.ECheese').checked;
+const ExtraChicken=document.querySelector('.EChicken').checked;
+const ExtraMayo=document.querySelector('.EMayo').checked;
+const quantity=document.getElementsByClassName('quantity')[0];
+const regularRadio=document.querySelector('.regular').checked;
+const mediumRadio=document.querySelector('.medium').checked;
+const largeRadio=document.querySelector('.large').checked;
+const ReceiptProName=document.getElementById('recieptProductName');
+const ReceiptQuantity=document.getElementById('recieptQuantity');
+const ReceiptTotal=document.getElementById('recieptTotal');
 
 
-let total='';
-total=600;
+let total=0;
+let Quantity=1;
 
-sum=(total,a)=>{total+=a;
-return alert(total)}
-
-myFunction1=(name)=>{modal.style.display="block";
+getNameOfPizza=(name)=>{modal.style.display="block";
+    selectionData.style.display="block"
+    replaceData.style.display="none"   
+    // disCodeInput.style.display="none"
     modalHeading.innerHTML=`${name}`;
     const orderNowBtn = document.getElementById('orderNowBtn');
-    Total.innerHTML=`Total:${total}`;
+    Total.innerHTML=`${total}`;
     desTotal.innerHTML=Total.innerHTML;
-    desHeading.innerHTML=modalHeading.innerHTML;}
+    desHeading.innerHTML=modalHeading.innerHTML;
+    
+    
+}
 
-getvalue= (value)=>{total+= value;
-        Total.innerHTML=`Total: ${total}`;
-        desTotal.innerHTML=Total.innerHTML;}
+getvalue= (value)=>{
+    total= value;
+        Total.innerHTML=` ${total}`;
+        desTotal.innerHTML=Total.innerHTML;
+    }
+getValueOfExtraToppings=(value)=>{
+    total+= value
+    Total.innerHTML=` ${total}`;
+    desTotal.innerHTML=Total.innerHTML;
+}  
 
- Order =() => {replaceData.style.display="block";
-    selectionData.style.display="none"}
+ModalOrderNow =() => {
+//     let checkboxes=document.getElementsByClassName('checkboxes')
+// if(checkboxes[0].checked){
+//     console.log('ok')
+// }
+
+// if(checkboxes[1].checked){
+//     console.log('ok1')
+// }
+// if(checkboxes[2].checked){
+//     console.log('ok2')
+// }
+    replaceData.style.display="block";
+    selectionData.style.display="none"
+    disCodeInput.style.display="none"
+    
+    ReceiptProName.innerHTML=modalHeading.innerHTML
+    ReceiptQuantity.innerHTML=` ${quantity.value} X `;
+    ReceiptTotal.innerHTML=Total.innerHTML
+    }
 
  Edit = ()=>{selectionData.style.display="block"
  replaceData.style.display="none"}
     
-displayModal = () => modal.style.display="none"
-
+hideModal = () => {modal.style.display="none";
+selectionData.style.display="none"
+replaceData.style.display="none"
+disCodeInput.style.display="none"
+}
 
 window.onclick=function(event){
     if(event.target==modal){
@@ -55,17 +98,21 @@ window.onclick=function(event){
     }
 }
 
-EnterCode=()=>{
+EnteredCode=(value)=>{
     disCodeSection.style.display="block"
     selectionData.style.display="none"
     replaceData.style.display="none"
     disCodeInput.value=''
     disCodeInput.focus()
+    code.innerHTML=value
+}
+getValueOfCode=(value)=>{
+    code.innerHTML=value
 }
 closeTasks=()=>{replaceData.style.display="block"
 disCodeSection.style.display="none"
 selectionData.style.display="none"}
-Enter=()=>{
+AfterCodeEnter=()=>{
     let userEnteredCode=disCodeInput.value
 
     if(userEnteredCode== '123A'){
@@ -84,9 +131,3 @@ Enter=()=>{
         return alert("Your Code doen`t Match with any discount code")
     }
 }
-
-
-
-
-
-
